e096436545972873bf37a6cc120ce6648abea7c0
chandra sekhar satyavarapu <sekharsatyavarapuc@gmail.com>
2020-07-22 01:16:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     SECRET_KEY=os.urandom(64)
 
     MONGODB_SETTINGS= {'db':'UTA_Enrollment',
-        'host':'mongodb+srv://admin:admin@cluster0.wrvu8.gcp.mongodb.net/UTA_Enrollment'
+        'host':DB_URI_KEY
     }
 
 #?retryWrites=true&w=majority
